67387482089c1d0ed847f788dee714e745b180f8
Mewurk <Mewurk@LMANTRAMEWURK13.Mantra.Local>
2022-02-17 15:59:23 +0530

    Added Reservation Pattern

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Patterns
 Repo to explore patterns
+
+1. 
+ReservationPattern
+===============
+Reference
+https://www.youtube.com/watch?v=PZm0RQGcs38
+
+The NServiceBus example using Saga is not fully explored.
---
+++ src/Reservation/ReserveSync/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Reservation;
+using ReservationSync;
+
+var services = new ServiceCollection();
+services.AddSingleton<FakeDatabase>();
+services.AddTransient<IUserRepository, UserRepository>();
+services.AddTransient<UserRegistration>();
+services.AddTransient<UsernameReservationSync>();
+var provider = services.BuildServiceProvider();
+
+var username = string.Empty;
+while (username != "q")
+{
+    Console.Write("Username: ");
+    username = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(username))
+    {
+        Console.WriteLine("Please type in a username.");
+        continue;
+    }
+
+    var userRegistration = provider.GetRequiredService<UserRegistration>();
+    var result = userRegistration.Register(username!);
+    Console.WriteLine(result ? "Registration Complete" : "Registration Failed");
+}
